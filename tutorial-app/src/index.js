e62527378b5d7ab85823db55b5c02ee318ac22cd
AkobirCoder <akobir0308@gmail.com>
2025-10-31 23:20:10 +0500

    ./tutorial-app/src/index.js was added

---
+++ tutorial-app/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // client and production
+import './index.css';
+// import App from './App';
+
+const name = "Akobir";
+
+const text = "How are you?";
+
+const block = (
+    <div 
+        style={{textAlign: "center"}}
+    >
+        <h1 
+            style={{fontSize: "5rem", color: "red", margin: "0rem"}} 
+            className='greeting' tabIndex={1}
+        >
+            Hi, {name}
+        </h1>
+        <p tabIndex={2}>
+            Text: {text} <br />
+            {10 * 10} <br /> 
+            {[1, 2, 3, 4, 5, 'bir', 'ikki', 'uch'].map((item, index) => {
+                <span key={index}>{item}</span>   
+            })}
+        </p> 
+        <input type="text" name="name" id="name" />
+        <button>Click</button>
+    </div>
+);
+
+// const el = React.createElement("h1", null, "Hello world!");
+
+const root = ReactDOM.createRoot(document.getElementById('element'));
+root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+
+    block
+);
