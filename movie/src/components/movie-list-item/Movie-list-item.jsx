3457eae19392903904eb6557fd726cfff4f27a1f
AkobirCoder <akobir0308@gmail.com>
2025-11-15 01:07:56 +0500

    ./movie/src/components/movie-list-item/Movie-list-item.jsx was updated

--- movie/src/components/movie-list-item/Movie-list-item.jsx
+++ movie/src/components/movie-list-item/Movie-list-item.jsx
@@ -1,34 +1,82 @@
+import { Component } from 'react';
 import './Movie-list-item.css';
 
-const MovieListItem = ({name, viewers, favourite}) => {
-    // console.log(favourite);
+class MovieListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favourite: false,
+            like: false,
+        }
+    }
 
-    // 2-usul: class qo'shish:
-    // let classNames = 'list-group-item d-flex justify-content-between';
+    onFavourite = () => {
+        this.setState(({favourite}) => ({
+            favourite: !favourite,
+        }));
+    }
 
-    // if (favourite) {
-    //     classNames += ' favourite';
-    // }
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like,
+        }));
+    }
+    
+    render() {
+        const {name, viewers} = this.props;
+        const {favourite, like} = this.state;
 
-    return (
-        // 2 - usul:
-        // <li className={classNames}>
-
-        // 1-usul: class qo'shish:
-        <li className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'}`}>
-            <span className='list-group-item-label'>{name}</span>
-            <input type="number" className='list-group-item-input' defaultValue={viewers} />
-            <div className='d-flex justify-content-center align-items-center'>
-                <button className='btn-sm btn-cookie' type='button'>
-                    <i className='fas fa-cookie'></i>
-                </button>
-                <button className='btn-sm btn-trash' type='button'>
-                    <i className='fas fa-trash'></i>
-                </button>
-                <i className='fas fa-star'></i>
-            </div>
-        </li>
-    );
+        return (
+            <li 
+                className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'} ${like && 'like'}`}
+            >
+                <span className='list-group-item-label' onClick={this.onLike}>
+                    {name}
+                </span>
+                <input type="number" className='list-group-item-input' defaultValue={viewers} />
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button className='btn-sm btn-cookie' type='button' onClick={this.onFavourite}>
+                        <i className='fas fa-cookie'></i>
+                    </button>
+                    <button className='btn-sm btn-trash' type='button'>
+                        <i className='fas fa-trash'></i>
+                    </button>
+                    <i className='fas fa-star'></i>
+                </div>
+            </li>
+        );
+    }
 }
+
+// const MovieListItem = ({name, viewers, favourite}) => {
+//     // console.log(favourite);
+
+//     // 2-usul: class qo'shish:
+//     // let classNames = 'list-group-item d-flex justify-content-between';
+
+//     // if (favourite) {
+//     //     classNames += ' favourite';
+//     // }
+
+//     return (
+//         // 2 - usul:
+//         // <li className={classNames}>
+
+//         // 1-usul: class qo'shish:
+//         <li className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'}`}>
+//             <span className='list-group-item-label'>{name}</span>
+//             <input type="number" className='list-group-item-input' defaultValue={viewers} />
+//             <div className='d-flex justify-content-center align-items-center'>
+//                 <button className='btn-sm btn-cookie' type='button'>
+//                     <i className='fas fa-cookie'></i>
+//                 </button>
+//                 <button className='btn-sm btn-trash' type='button'>
+//                     <i className='fas fa-trash'></i>
+//                 </button>
+//                 <i className='fas fa-star'></i>
+//             </div>
+//         </li>
+//     );
+// }
 
 export default MovieListItem;
