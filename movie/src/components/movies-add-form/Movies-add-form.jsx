29d50aa8580aa801a0bcf747abb85ee8fbadb868
AkobirCoder <akobir0308@gmail.com>
2025-11-19 00:12:43 +0500

    ./movie/src/components/movies-add-form/Movies-add-form.jsx was updated

--- movie/src/components/movies-add-form/Movies-add-form.jsx
+++ movie/src/components/movies-add-form/Movies-add-form.jsx
@@ -2,49 +2,20 @@ import { Component } from 'react';
 import './Movies-add-form.css';
 
 class MoviesAddForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '', // default qiymat, boshqarib bo'ladigan form uchun
-            views: '', // default qiymat, boshqarib bo'ladigan form uchun
-        }
-    }
-
-    changeHandlerInput = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        });
-    }
-
-    addFormHandler = (event) => {
-        event.preventDefault();
-
-        const {addForm} = this.props;
-        const {name, views} = this.state;
-
-        addForm({
-            name: name, viewers: views,
-        });
-
-        this.setState({
-            name: '',
-            views: '',
-        })
-    }
+    
 
     render() {
-        const {name, views} = this.state;
-        const {changeHandlerInput, addFormHandler} = this;
+        
 
         return (
             <div className='movies-add-form'>
                 <h3>Yangi kino qo'shish</h3>
-                <form className='add-form d-flex' onSubmit={addFormHandler}>
+                <form className='add-form d-flex'>
                     <input type="text" className='form-control mx-1 new-post-label' placeholder='Qanday kino?' 
-                        onChange={changeHandlerInput} name='name' value={name}
+                        
                     />
                     <input type="number" className='form-control mx-1 new-post-label' placeholder="Nechi marotaba ko'rilgan?" 
-                        onChange={this.changeHandlerInput} name='views' value={views}
+                        
                     />
                     <button className='btn btn-outline-dark ms-1' type='submit'>
                         Qo'shish
