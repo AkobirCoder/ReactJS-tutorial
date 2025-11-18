74f6d0b7f48e119cc0e75d3ad70c9348b07300e2
AkobirCoder <akobir0308@gmail.com>
2025-11-19 00:38:16 +0500

    ./movie/src/components/movies-add-form/Movies-add-form.jsx was updated

--- movie/src/components/movies-add-form/Movies-add-form.jsx
+++ movie/src/components/movies-add-form/Movies-add-form.jsx
@@ -2,20 +2,33 @@ import { Component } from 'react';
 import './Movies-add-form.css';
 
 class MoviesAddForm extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '+998()-', // inputga value atributi orqali berilsa default qiymat browserda ko'rinadi
+            views: '', // inputga value atributi orqali berilsa default qiymat browserda ko'rinadi
+        }
+    }
+
+    changeHandlerInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
 
     render() {
-        
+        const {name, views} = this.state;
+        const {changeHandlerInput} = this;
 
         return (
             <div className='movies-add-form'>
                 <h3>Yangi kino qo'shish</h3>
                 <form className='add-form d-flex'>
                     <input type="text" className='form-control mx-1 new-post-label' placeholder='Qanday kino?' 
-                        
+                        onChange={changeHandlerInput} name='name' value={name}
                     />
                     <input type="number" className='form-control mx-1 new-post-label' placeholder="Nechi marotaba ko'rilgan?" 
-                        
+                        onChange={changeHandlerInput} name='views' value={views}
                     />
                     <button className='btn btn-outline-dark ms-1' type='submit'>
                         Qo'shish
