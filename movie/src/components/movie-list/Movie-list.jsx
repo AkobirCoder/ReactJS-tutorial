2c634657cd9f6317d5f353a420b10d9321247c76
AkobirCoder <akobir0308@gmail.com>
2025-11-19 22:39:53 +0500

    ./movie/src/components/movie-list/Movie-list.jsx was updated

--- movie/src/components/movie-list/Movie-list.jsx
+++ movie/src/components/movie-list/Movie-list.jsx
@@ -1,18 +1,43 @@
+import { Component } from 'react';
 import MovieListItem from '../movie-list-item/Movie-list-item';
 
 import './Movie-list.css';
 
-const MovieList = ({data, onDelete}) => {
-    return (
-        <ul className='movie-list'>
-            {
-                data.map(item => (
-                    <MovieListItem key={item.id} name={item.name} viewers={item.viewers} favourite={item.favourite} onDelete={() => onDelete(item.id)} />
-                    // <MovieListItem {...item} />
-                ))
-            }
-        </ul>
-    );
+class MovieList extends Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        const {data} = this.props;
+
+        return (
+            <ul className='movie-list'>
+                {
+                    data.map(item => (
+                        <MovieListItem key={item.id} name={item.name} viewers={item.viewers} favourite={item.favourite} />
+                        // <MovieListItem {...item} />
+                    ))
+                }
+            </ul>
+        );
+    }
 }
+
+// const MovieList = ({data, onDelete}) => {
+//     return (
+//         <ul className='movie-list'>
+//             {
+//                 data.map(item => (
+//                     <MovieListItem key={item.id} name={item.name} viewers={item.viewers} favourite={item.favourite} onDelete={() => onDelete(item.id)} />
+//                     // <MovieListItem {...item} />
+//                 ))
+//             }
+//         </ul>
+//     );
+// }
 
 export default MovieList;
