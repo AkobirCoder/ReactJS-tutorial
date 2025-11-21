7f1983b53d7eb7366a463d0980f1bca0f26f5c5e
AkobirCoder <akobir0308@gmail.com>
2025-11-22 00:38:30 +0500

    ./movie/src/components/movie-list-item/Movie-list-item.jsx was updated

--- movie/src/components/movie-list-item/Movie-list-item.jsx
+++ movie/src/components/movie-list-item/Movie-list-item.jsx
@@ -27,7 +27,8 @@ class MovieListItem extends Component {
     }
     
     render() {
-        const {name, viewers, favourite, like, onDelete, onToggleFavourite, onToggleLike} = this.props;
+        // const {name, viewers, favourite, like, onDelete, onToggleFavourite, onToggleLike} = this.props;
+        const {name, viewers, favourite, like, onDelete, onToggleProp} = this.props;
         // const {favourite, like} = this.state;
         // const {onFavourite, onLike} = this;
 
@@ -36,13 +37,15 @@ class MovieListItem extends Component {
                 className={`list-group-item d-flex justify-content-between ${favourite && 'favourite'} ${like && 'like'}`}
             >
                 {/* <span className='list-group-item-label' onClick={onLike}> */}
-                <span className='list-group-item-label' onClick={onToggleLike}>
+                {/* <span className='list-group-item-label' onClick={onToggleLike}> */}
+                <span className='list-group-item-label' data-toggle='like' onClick={onToggleProp}>
                     {name}
                 </span>
                 <input type="number" className='list-group-item-input' defaultValue={viewers} />
                 <div className='d-flex justify-content-center align-items-center'>
                     {/* <button className='btn-sm btn-cookie' type='button' onClick={onFavourite}> */}
-                    <button className='btn-sm btn-cookie' type='button' onClick={onToggleFavourite}>
+                    {/* <button className='btn-sm btn-cookie' type='button' onClick={onToggleFavourite}> */}
+                    <button className='btn-sm btn-cookie' type='button' data-toggle='favourite' onClick={onToggleProp}>
                         <i className='fas fa-cookie'></i>
                     </button>
                     <button className='btn-sm btn-trash' type='button' onClick={onDelete}>
