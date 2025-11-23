10f8d511db5ec052e3a90f6349e257703f72f979
AkobirCoder <akobir0308@gmail.com>
2025-11-23 16:17:40 +0500

    ./movie/src/components/search-panel/Search-panel.jsx was updated

--- movie/src/components/search-panel/Search-panel.jsx
+++ movie/src/components/search-panel/Search-panel.jsx
@@ -2,9 +2,29 @@ import { Component } from 'react';
 import './Search-panel.css';
 
 class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+        }
+    }
+
+    updateTermHandler = (e) => {
+        const term = e.target.value.toLowerCase();
+
+        this.setState({term});
+
+        const {updateTermHandler} = this.props;
+
+        updateTermHandler(term);
+    }
+
     render() {
+        const {term} = this.state;
+        const {updateTermHandler} = this;
+
         return (
-            <input type="text" className="form-control search-input" placeholder="Kinolarni qidirish" />
+            <input type="text" className="form-control search-input" placeholder="Kinolarni qidirish" onChange={updateTermHandler} value={term} />
         );
     }
 }
