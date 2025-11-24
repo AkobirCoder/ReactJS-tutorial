7012704de366571f0deec43142c2d46f3356642c
AkobirCoder <akobir0308@gmail.com>
2025-11-24 20:25:40 +0500

    ./movie/src/components/app-filter/App-filter.jsx was updated

--- movie/src/components/app-filter/App-filter.jsx
+++ movie/src/components/app-filter/App-filter.jsx
@@ -1,40 +1,79 @@
+import { Component } from 'react';
 import './App-filter.css'
 
-const AppFilter = ({updateFilterHandler, filter}) => {
-    const btnsArray = [
-        {name: 'all', label: "Barcha kinolar"},
-        {name: 'popular', label: "Mashhur kinolar"},
-        {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
-    ];
+class AppFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-    return (
-        <div className="btn-group">
-            {
-                btnsArray.map((btn) => {
-                    return (
-                        <button 
-                            className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
-                            type='button' 
-                            key={btn.name} 
-                            onClick={() => updateFilterHandler(btn.name)}
-                        >
-                            {btn.label}
-                        </button>
-                    );
-                })
-            }
+        }
+    }
 
-            {/* <button className="btn btn-dark" type="button">
-                Barcha kinolar
-            </button>
-            <button className="btn btn-outline-dark" type="button">
-                Mashhur kinolar
-            </button>
-            <button className="btn btn-outline-dark" type="button">
-                Eng ko'p ko'rilgan kinolar
-            </button> */}
-        </div>
-    );
+    render() {
+        const btnsArray = [
+            {name: 'all', label: "Barcha kinolar"},
+            {name: 'popular', label: "Mashhur kinolar"},
+            {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
+        ];
+
+        const {updateFilterHandler, filter} = this.props;
+
+        return (
+            <div className="btn-group">
+                {
+                    btnsArray.map((btn) => {
+                        return (
+                            <button 
+                                className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
+                                type='button' 
+                                key={btn.name} 
+                                onClick={() => updateFilterHandler(btn.name)}
+                            >
+                                {btn.label}
+                            </button>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
 }
+
+// const AppFilter = ({updateFilterHandler, filter}) => {
+//     const btnsArray = [
+//         {name: 'all', label: "Barcha kinolar"},
+//         {name: 'popular', label: "Mashhur kinolar"},
+//         {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
+//     ];
+
+//     return (
+//         <div className="btn-group">
+//             {
+//                 btnsArray.map((btn) => {
+//                     return (
+//                         <button 
+//                             className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
+//                             type='button' 
+//                             key={btn.name} 
+//                             onClick={() => updateFilterHandler(btn.name)}
+//                         >
+//                             {btn.label}
+//                         </button>
+//                     );
+//                 })
+//             }
+
+//             {/* <button className="btn btn-dark" type="button">
+//                 Barcha kinolar
+//             </button>
+//             <button className="btn btn-outline-dark" type="button">
+//                 Mashhur kinolar
+//             </button>
+//             <button className="btn btn-outline-dark" type="button">
+//                 Eng ko'p ko'rilgan kinolar
+//             </button> */}
+//         </div>
+//     );
+// }
 
 export default AppFilter;
