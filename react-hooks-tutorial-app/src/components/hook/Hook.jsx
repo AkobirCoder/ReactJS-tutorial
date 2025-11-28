2e88fec44ae0b6c163ea2a2050059e1e7dfccf43
AkobirCoder <akobir0308@gmail.com>
2025-11-29 00:07:47 +0500

    ./react-hooks-tutorial-app/src/components/hook/Hook.jsx was updated

--- react-hooks-tutorial-app/src/components/hook/Hook.jsx
+++ react-hooks-tutorial-app/src/components/hook/Hook.jsx
@@ -6,7 +6,17 @@ const User = ({firstName, lastName, link}) => {
     const [counter, setCount] = useState(0);
     const [age, setAge] = useState(21);
 
-    console.log(counter);
+    const onIncrement = () => {
+        setCount(counter + 1);
+    }
+
+    const onDecrement = () => {
+        setCount(counter - 1);
+    }
+
+    const onRestart = () => {
+        setCount(0);
+    }
 
     return (
         <div>
@@ -16,6 +26,12 @@ const User = ({firstName, lastName, link}) => {
                     Mening Telegram profilim.
                 </a>
                 <p>{counter}</p>
+                <input type="text" />
+                <div>
+                    <button onClick={onIncrement}>+</button>
+                    <button onClick={onDecrement}>-</button>
+                    <button onClick={onRestart}>0</button>
+                </div>
             </div>
         </div>
     );
