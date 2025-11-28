363ea1dbc0f45fcd1a3463b4094b43596156d0c2
AkobirCoder <akobir0308@gmail.com>
2025-11-28 23:55:49 +0500

    ./react-hooks-tutorial-app/src/components/hook/Hook.jsx was updated

--- react-hooks-tutorial-app/src/components/hook/Hook.jsx
+++ react-hooks-tutorial-app/src/components/hook/Hook.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 import './Hook.css';
 
 const User = ({firstName, lastName, link}) => {
+    const [counter, setCount] = useState(0);
+    const [age, setAge] = useState(21);
+
+    console.log(counter);
+
     return (
         <div>
             <div>
-                <h1>Mening ismim - {firstName}, familiyam - {lastName}.</h1>
+                <h1>Mening ismim - {firstName}, familiyam - {lastName}, yoshim - {age} da.</h1>
                 <a href={link} target='_blank' rel='noreferrer'>
                     Mening Telegram profilim.
                 </a>
+                <p>{counter}</p>
             </div>
         </div>
     );
