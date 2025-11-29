6486edb2ec2664ee27bb976aa212e7b58baf7887
AkobirCoder <akobir0308@gmail.com>
2025-11-29 11:21:06 +0500

    ./react-hooks-tutorial-app/src/components/hook/Hook.jsx was updated

--- react-hooks-tutorial-app/src/components/hook/Hook.jsx
+++ react-hooks-tutorial-app/src/components/hook/Hook.jsx
@@ -3,59 +3,99 @@ import { useState } from 'react';
 import './Hook.css';
 
 const User = ({firstName, lastName, link, login}) => {
-    const [counter, setCount] = useState(0);
-    const [age, setAge] = useState(21);
-    const [isLogin, setIsLogin] = useState(false);
+    // const [counter, setCount] = useState(0);
+    // const [age, setAge] = useState(21);
+    // const [isLogin, setIsLogin] = useState(false);
 
-    // const onIncrement = () => {
-    //     setCount(counter + 1);
-    // }
+    // // const onIncrement = () => {
+    // //     setCount(counter + 1);
+    // // }
 
 
-    // `prevState` ya'ni avvalgi holati holati orqali o'zgartirish:
+    // // `prevState` ya'ni avvalgi holati holati orqali o'zgartirish:
 
-    // const onIncrement = () => {
-    //     setCount(prevState => {
-    //         return prevState + 1;
-    //     });
-    // }
+    // // const onIncrement = () => {
+    // //     setCount(prevState => {
+    // //         return prevState + 1;
+    // //     });
+    // // }
 
-    // const onIncrement = () => {
-    //     setCount(counter + 1);
-    //     setCount(counter + 1); // bu ikkala setCount funksiyasi bitta bo'lgani uchun 100 marta chaqirilsa ham bir marta ishlaydi.
-    // }
+    // // const onIncrement = () => {
+    // //     setCount(counter + 1);
+    // //     setCount(counter + 1); // bu ikkala setCount funksiyasi bitta bo'lgani uchun 100 marta chaqirilsa ham bir marta ishlaydi.
+    // // }
 
     
-    // counter +2 ga o'zgaradi:
+    // // counter +2 ga o'zgaradi:
     
-    const onIncrement = () => {
-        setCount(prevState => prevState + 1);
-        setCount(prevState => prevState + 1); // ammo `prevState` orqali o'zgartirilsa avvalgi holatini o'zgartirib nechi marta chaqirilsa o'shancha marta o'zgaradi.
-    }
+    // const onIncrement = () => {
+    //     setCount(prevState => prevState + 1);
+    //     setCount(prevState => prevState + 1); // ammo `prevState` orqali o'zgartirilsa avvalgi holatini o'zgartirib nechi marta chaqirilsa o'shancha marta o'zgaradi.
+    // }
+
+    // // const onDecrement = () => {
+    // //     setCount(counter - 1);
+    // // }
 
     // const onDecrement = () => {
-    //     setCount(counter - 1);
+    //     setCount(prevState => (prevState - 1));
+    //     setCount(prevState => (prevState - 1));
     // }
 
-    const onDecrement = () => {
-        setCount(prevState => (prevState - 1));
-        setCount(prevState => (prevState - 1));
-    }
+    // const onRestart = () => {
+    //     setCount(0);
+    // }
 
-    const onRestart = () => {
-        setCount(0);
-    }
+    // const onAgeHandler = (e) => {
+    //     setAge(e.target.value);
+    // }
 
-    const onAgeHandler = (e) => {
-        setAge(e.target.value);
-    }
-
-    const onToggleLogin = () => {
-        setIsLogin(prevState => !prevState);
-    }
+    // const onToggleLogin = () => {
+    //     setIsLogin(prevState => !prevState);
+    // }
 
 
     // Hooklarda obyektlardan foydalanish:
+
+    const [state, setState] = useState({
+        counter: 0,
+        age: 21,
+        isLogin: false,
+    });
+
+    console.log(state);
+
+    const onIncrement = () => {
+        setState(prevState => ({
+            ...prevState, counter: prevState.counter + 1,
+        }));
+    }
+
+    const onDecrement = () => {
+        setState(prevState => ({
+            ...prevState, counter: prevState.counter - 1,
+        }));
+    }
+
+    const onRestart = () => {
+        setState(prevState => ({
+            ...prevState, counter: 0,
+        }));
+    }
+
+    const onAgeHandler = (e) => {
+        setState(prevState => ({
+            ...prevState, age: e.target.value,
+        }));
+    }
+
+    const onToggleLogin = () => {
+        setState(prevState => ({
+            ...prevState, isLogin: !prevState.isLogin,
+        }));
+    }
+
+    const {counter, age, isLogin} = state;
 
     return (
         <div>
