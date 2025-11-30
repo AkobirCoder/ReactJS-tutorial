c3d25eed804717b269d07b696cfea4d185db1ddc
AkobirCoder <akobir0308@gmail.com>
2025-11-30 23:27:00 +0500

    ./react-hooks-tutorial-app/src/components/app/App.jsx was updated

--- react-hooks-tutorial-app/src/components/app/App.jsx
+++ react-hooks-tutorial-app/src/components/app/App.jsx
@@ -1,16 +1,28 @@
-import User from '../hook/Hook';
+// import User from '../hook/Hook';
 
 import { Fragment } from 'react/jsx-runtime';
 import './App.css';
 import State from '../state/State';
+import { useState } from 'react';
 
 const App = () => {
+    const [isDisplay, setIsDisplay] = useState(true);
+
+    const deleteDisplayHandler = () => {
+        setIsDisplay(false);
+    }
+
     return (
         <Fragment>
-            <User firstName={"Akobir"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} />
+            {/* <User firstName={"Akobir"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} /> */}
 
             {/* Lifecycle method: */}
-            <State firstName={"Azizbek"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} />
+            {/* <State firstName={"Azizbek"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} /> */}
+            <button className='btn btn-success' onClick={deleteDisplayHandler}>diplay</button>
+            {isDisplay
+                ? <State firstName={"Azizbek"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} />
+                : null
+            }
         </Fragment>
     );
 }
