ec3c3d67309b1f2974cd843d8253f3444939bcbd
AkobirCoder <akobir0308@gmail.com>
2025-11-30 21:59:10 +0500

    ./react-hooks-tutorial-app/src/components/app/App.jsx was updated

--- react-hooks-tutorial-app/src/components/app/App.jsx
+++ react-hooks-tutorial-app/src/components/app/App.jsx
@@ -2,11 +2,13 @@ import User from '../hook/Hook';
 
 import { Fragment } from 'react/jsx-runtime';
 import './App.css';
+import State from '../state/State';
 
 const App = () => {
     return (
         <Fragment>
             <User firstName={"Akobir"} lastName={"Usmonov"} link={"https://t.me/csr0308"} login={"Login"} />
+            <State />
         </Fragment>
     );
 }
