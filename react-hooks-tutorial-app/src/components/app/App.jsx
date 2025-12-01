84b342074de2995cb61a7ad5786b8c10a1d483e5
AkobirCoder <akobir0308@gmail.com>
2025-12-01 23:44:42 +0500

    ./react-hooks-tutorial-app/src/components/app/App.jsx was updated

--- react-hooks-tutorial-app/src/components/app/App.jsx
+++ react-hooks-tutorial-app/src/components/app/App.jsx
@@ -1,4 +1,4 @@
-// import User from '../hook/Hook';
+import User from '../hooks/UseStateHook';
 
 import { Fragment } from 'react/jsx-runtime';
 import './App.css';
@@ -14,7 +14,7 @@ const App = () => {
 
     return (
         <Fragment>
-            {/* <User firstName={"Akobir"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} /> */}
+            <User firstName={"Akobir"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} />
 
             {/* Lifecycle method: */}
             {/* <State firstName={"Azizbek"} lastName={"Usmonov"} link={"https://t.me/c_s_p0308"} login={"Login"} /> */}
