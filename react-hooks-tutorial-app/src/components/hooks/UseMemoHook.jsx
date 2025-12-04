4314bb1720fd2e0ea06061f788b36184844a5740
AkobirCoder <akobir0308@gmail.com>
2025-12-04 12:54:56 +0500

    ./react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx was updated

--- react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx
+++ react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const UseMemoHook = () => {
     return (
-        <div className='container'>
+        <div className='container mb-5'>
             <code>useMemo hook:</code>
-            
+            <div className='w-75 p-5 mt-5 mx-auto border rounded-3'>
+                <h1 className='fs-3 fw-light text-center'>useMemo hook</h1>
+            </div>
         </div>
     );
 }
