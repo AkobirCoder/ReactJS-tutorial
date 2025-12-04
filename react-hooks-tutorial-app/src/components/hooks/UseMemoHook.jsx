f17c1b163f35f4ae29748c0c192bd4556d2f21d7
AkobirCoder <akobir0308@gmail.com>
2025-12-04 12:49:31 +0500

    ./react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx was updated

---
+++ react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UseMemoHook = () => {
+    return (
+        <div className='container'>
+            <code>useMemo hook:</code>
+            
+        </div>
+    );
+}
+
+export default UseMemoHook;
