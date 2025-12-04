819bdb39aac7aa64dd5a2693cd18b40dd79ec536
AkobirCoder <akobir0308@gmail.com>
2025-12-04 23:16:12 +0500

    ./react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx was updated

--- react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx
+++ react-hooks-tutorial-app/src/components/hooks/UseMemoHook.jsx
@@ -1,4 +1,16 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+
+const bigCountNumber = (number) => {
+    // console.log('Render');
+
+    let i = 0;
+
+    while (i < 1000000000) {
+        i++;
+    } 
+
+    return number * 2;
+}
 
 const UseMemoHook = () => {
     const [counter, setCount] = useState(0);
@@ -21,14 +33,18 @@ const UseMemoHook = () => {
         });
     }
 
+    const number = useMemo(() => {
+        return bigCountNumber(counter);
+    }, [counter]);
+
     return (
         <div className='container mb-5'>
             <code>useMemo hook:</code>
             <div className='w-75 p-5 mt-5 mx-auto border rounded-3'>
                 <h1 className='fs-3 fw-light text-center'>useMemo hook:</h1>
-                <p style={colors} className='text-center'>User activated {counter}</p>
+                <p style={colors} className='text-center'>User activated {number}</p>
                 <div className="d-flex justify-content-center">
-                    <button className="btn btn-success mx-2" type="button" onClick={onIncrement}>
+                    <button className="btn btn-success mx-2" type="button" name='inc' onClick={onIncrement}>
                         Increase
                     </button>
                     <button className="btn btn-warning mx-2" type="button" onClick={onToggle}>
