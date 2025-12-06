5ddad6823a035c92794fbfab177f146775eaf7fc
AkobirCoder <akobir0308@gmail.com>
2025-12-06 23:53:23 +0500

    ./movie/src/components/app/App.jsx was updated

--- movie/src/components/app/App.jsx
+++ movie/src/components/app/App.jsx
@@ -92,6 +92,10 @@ const App = () => {
         setFilter(filter);
     }
 
+    const allMoviesCount = data.length;
+    const favouriteMoviesCount = data.filter((item) => item.favourite).length;
+    const visibleData = filterHandler(searchHandler(data, term), filter);
+
     return (
         <div className="app font-monospace" onClick={() => this.setState({errorMessage: ''})}>
             <div className={`error-message ${errorMessage ? 'd-flex' : 'd-none'}`}>
@@ -111,271 +115,269 @@ const App = () => {
                     <SearchPanel updateTermHandler={updateTermHandler} />
                     <AppFilter filter={filter} updateFilterHandler={updateFilterHandler} />
                 </div>
-                {/* <MovieList data={data} onDelete={onDelete} onToggleFavourite={onToggleFavourite} onToggleLike={onToggleLike} /> */}
-                {/* <MovieList data={data} onDelete={onDelete} onToggleProp={onToggleProp} /> */}
-                {
+                {/* {
                     finalNoResultMessage && filteredByData.length === 0 
                     ? (<div className="fs-5 result-message">{finalNoResultMessage}</div>) 
                     : <MovieList data={visibleData} onDelete={onDelete} onToggleProp={onToggleProp} />
-                }
-                
+                } */}
+                <MovieList data={visibleData} onDelete={onDelete} onToggleProp={onToggleProp} />
                 <MoviesAddForm addForm={addForm} />
             </div>
         </div>
     );
 }
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [
-                { name: "Empire of Osman", viewers: 988, favourite: false, like: false, id: 1, },
-                { name: "Ertugrul", viewers: 789, favourite: false, like: false, id: 2, },
-                { name: "Omar", viewers: 1091, favourite: false, like: false, id: 3, },
-            ],
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: [
+//                 { name: "Empire of Osman", viewers: 988, favourite: false, like: false, id: 1, },
+//                 { name: "Ertugrul", viewers: 789, favourite: false, like: false, id: 2, },
+//                 { name: "Omar", viewers: 1091, favourite: false, like: false, id: 3, },
+//             ],
 
-            // Search-paneldagi inputning qiymatini saqlab turuvchi state:
-            term: '',
+//             // Search-paneldagi inputning qiymatini saqlab turuvchi state:
+//             term: '',
 
-            // App-filterdagi filter qiluvchi buttonlar uchun state:
-            filter: 'all',
+//             // App-filterdagi filter qiluvchi buttonlar uchun state:
+//             filter: 'all',
 
-            errorMessage: '',
-        }
+//             errorMessage: '',
+//         }
 
-        // this.maxId = 4;
-    }
+//         // this.maxId = 4;
+//     }
 
-    onDelete = (id) => {
+//     onDelete = (id) => {
 
-        // Mutable:
+//         // Mutable:
 
-        // this.setState(({data}) => {
-        //     const index = data.findIndex(item => item.id !== id);
-        //     data.splice(index, 1);
+//         // this.setState(({data}) => {
+//         //     const index = data.findIndex(item => item.id !== id);
+//         //     data.splice(index, 1);
 
-        //     return {
-        //         data,
-        //     }
-        // });
-
-
-        // Immutable:
-
-        this.setState(({data}) => {
-            // const newArray = data.filter((item) => {
-            //     return item.id !== id;
-            // });
-
-            // return {
-            //     data: newArray,
-            // }
-
-            return {
-                data: data.filter(item => item.id !== id),
-            }
-        });
-    }
-
-    addForm = (item) => {
-        // const newItem = {name: item.name, viewers: item.viewers, id: this.maxId++}
-
-        const newItem = {name: item.name, viewers: item.viewers, id: uuidv4(), favourite: false, like: false}
-
-        if (!item.name || !item.viewers) {
-            // alert("Iltimos kino kiriting");
-
-            // return;
-
-            this.setState(() => {
-                return {
-                    errorMessage: 'Iltimos kino kiriting',
-                }
-            });
-
-            setTimeout(() => {
-                this.setState(() => {
-                    return {
-                        errorMessage: '',
-                    }
-                });
-            }, 6000);
-
-            return;
-        }
-
-        this.setState(({data}) => {
-            // const newArray = [...data, {...item, id: uuidv4()}];
-
-            // return {
-            //     data: newArray,
-            // }
-
-            return {
-                // data: [...data, {...item, id: uuidv4()}],
-
-                data: [...data, newItem],
-            }
-        });
-    }
-
-    onToggleFavourite = (id) => {
-        this.setState(({data}) => {
-            // const newArray = data.map((item) => {
-            //     if (item.id === id) {
-            //         return {...item, favourite: !item.favourite}
-            //     }
-
-            //     return item;
-            // });
-
-            // return {
-            //     data: newArray,
-            // }
-
-            // return {
-            //     data: data.map((item) => {
-            //         if (item.id === id) {
-            //             return {...item, favourite: !item.favourite}
-            //         } else {
-            //             return item;
-            //         }
-            //     }),
-            // }
-        });
+//         //     return {
+//         //         data,
+//         //     }
+//         // });
 
 
-        // Ternary operator:
+//         // Immutable:
 
-        // this.setState(({data}) => ({
-        //     data: data.map((item) => item.id === id ? {...item, favourite: !item.favourite} : item),
-        // }));
-    }
+//         this.setState(({data}) => {
+//             // const newArray = data.filter((item) => {
+//             //     return item.id !== id;
+//             // });
 
-    onToggleLike = (id) => {
-        this.setState(({data}) => {
-            // const newArray = data.map((item) => {
-            //     if (item.id === id) {
-            //         return {...item, like: !item.like}
-            //     }
+//             // return {
+//             //     data: newArray,
+//             // }
 
-            //     return item;
-            // });
+//             return {
+//                 data: data.filter(item => item.id !== id),
+//             }
+//         });
+//     }
+
+//     addForm = (item) => {
+//         // const newItem = {name: item.name, viewers: item.viewers, id: this.maxId++}
+
+//         const newItem = {name: item.name, viewers: item.viewers, id: uuidv4(), favourite: false, like: false}
+
+//         if (!item.name || !item.viewers) {
+//             // alert("Iltimos kino kiriting");
+
+//             // return;
+
+//             this.setState(() => {
+//                 return {
+//                     errorMessage: 'Iltimos kino kiriting',
+//                 }
+//             });
+
+//             setTimeout(() => {
+//                 this.setState(() => {
+//                     return {
+//                         errorMessage: '',
+//                     }
+//                 });
+//             }, 6000);
+
+//             return;
+//         }
+
+//         this.setState(({data}) => {
+//             // const newArray = [...data, {...item, id: uuidv4()}];
+
+//             // return {
+//             //     data: newArray,
+//             // }
+
+//             return {
+//                 // data: [...data, {...item, id: uuidv4()}],
+
+//                 data: [...data, newItem],
+//             }
+//         });
+//     }
+
+//     onToggleFavourite = (id) => {
+//         this.setState(({data}) => {
+//             // const newArray = data.map((item) => {
+//             //     if (item.id === id) {
+//             //         return {...item, favourite: !item.favourite}
+//             //     }
+
+//             //     return item;
+//             // });
+
+//             // return {
+//             //     data: newArray,
+//             // }
+
+//             // return {
+//             //     data: data.map((item) => {
+//             //         if (item.id === id) {
+//             //             return {...item, favourite: !item.favourite}
+//             //         } else {
+//             //             return item;
+//             //         }
+//             //     }),
+//             // }
+//         });
+
+
+//         // Ternary operator:
+
+//         // this.setState(({data}) => ({
+//         //     data: data.map((item) => item.id === id ? {...item, favourite: !item.favourite} : item),
+//         // }));
+//     }
+
+//     onToggleLike = (id) => {
+//         this.setState(({data}) => {
+//             // const newArray = data.map((item) => {
+//             //     if (item.id === id) {
+//             //         return {...item, like: !item.like}
+//             //     }
+
+//             //     return item;
+//             // });
         
-            // return {
-            //     data: newArray,
-            // }
+//             // return {
+//             //     data: newArray,
+//             // }
 
-            // return {
-            //     data: data.map((item) => {
-            //         if (item.id === id) {
-            //             return {...item, like: !item.like}
-            //         } else {
-            //             return item;
-            //         }
-            //     }),
-            // }
-        });
+//             // return {
+//             //     data: data.map((item) => {
+//             //         if (item.id === id) {
+//             //             return {...item, like: !item.like}
+//             //         } else {
+//             //             return item;
+//             //         }
+//             //     }),
+//             // }
+//         });
 
 
-        // Ternary operator:
+//         // Ternary operator:
 
-        // this.setState(({data}) => ({
-        //     data: data.map((item) => item.id === id ? {...item, like: !item.like} : item),
-        // }));
-    }
+//         // this.setState(({data}) => ({
+//         //     data: data.map((item) => item.id === id ? {...item, like: !item.like} : item),
+//         // }));
+//     }
     
 
-    onToggleProp = (id, prop) => {
-        // console.log(prop);
+//     onToggleProp = (id, prop) => {
+//         // console.log(prop);
 
-        this.setState(({data}) => ({
-            data: data.map((item) => {
-                if (item.id === id) {
-                    return {...item, [prop]: !item[prop]}
-                }
+//         this.setState(({data}) => ({
+//             data: data.map((item) => {
+//                 if (item.id === id) {
+//                     return {...item, [prop]: !item[prop]}
+//                 }
 
-                return item;
-            }),
-        }));
-    }
+//                 return item;
+//             }),
+//         }));
+//     }
     
-    searchHandler = (array, term) => {
-        if (term.length === 0) {
-            return array;
-        }
+//     searchHandler = (array, term) => {
+//         if (term.length === 0) {
+//             return array;
+//         }
 
-        return array.filter((item) => {
-            return item.name.toLowerCase().indexOf(term) > -1;
-        });
-    }
+//         return array.filter((item) => {
+//             return item.name.toLowerCase().indexOf(term) > -1;
+//         });
+//     }
 
-    updateTermHandler = (term) => {
-        this.setState({term});
-    }
+//     updateTermHandler = (term) => {
+//         this.setState({term});
+//     }
 
-    filterHandler = (array, filter) => {
-        switch (filter) {
-            case 'popular':
-                return array.filter((item) => {
-                    return item.like;
-                });
-            case 'mostViewers':
-                return array.filter((item) => {
-                    return item.viewers > 800;
-                });
-            default:
-                return array;
-        }
-    }
+//     filterHandler = (array, filter) => {
+//         switch (filter) {
+//             case 'popular':
+//                 return array.filter((item) => {
+//                     return item.like;
+//                 });
+//             case 'mostViewers':
+//                 return array.filter((item) => {
+//                     return item.viewers > 800;
+//                 });
+//             default:
+//                 return array;
+//         }
+//     }
 
-    updateFilterHandler = (filter) => {
-        this.setState({filter});
-    }
+//     updateFilterHandler = (filter) => {
+//         this.setState({filter});
+//     }
 
-    render() {
-        const {data, term, filter, errorMessage} = this.state;
-        // const {onDelete, addForm, onToggleFavourite, onToggleLike} = this;
-        const {onDelete, addForm, onToggleProp, searchHandler, updateTermHandler, filterHandler, updateFilterHandler} = this;
-        const allMoviesCount = data.length;
-        const favouriteMoviesCount = data.filter((item) => item.favourite).length;
-        const filteredByData = searchHandler(data, term);
-        const visibleData = filterHandler(filteredByData, filter);
-        const finalNoResultMessage = filteredByData.length === 0 && term ? "Kino mavjud emas, iltimos to'g'ri nom kiriting" : "";
+//     render() {
+//         const {data, term, filter, errorMessage} = this.state;
+//         // const {onDelete, addForm, onToggleFavourite, onToggleLike} = this;
+//         const {onDelete, addForm, onToggleProp, searchHandler, updateTermHandler, filterHandler, updateFilterHandler} = this;
+//         const allMoviesCount = data.length;
+//         const favouriteMoviesCount = data.filter((item) => item.favourite).length;
+//         const filteredByData = searchHandler(data, term);
+//         const visibleData = filterHandler(filteredByData, filter);
+//         const finalNoResultMessage = filteredByData.length === 0 && term ? "Kino mavjud emas, iltimos to'g'ri nom kiriting" : "";
 
-        return (
-            <div className="app font-monospace" onClick={() => this.setState({errorMessage: ''})}>
-                <div className={`error-message ${errorMessage ? 'd-flex' : 'd-none'}`}>
-                    {errorMessage && 
-                        <div className="fs-4 message" onClick={(e) => e.stopPropagation()}>
-                            <i 
-                                className="fa-solid fa-x fs-6 text-white" 
-                                onClick={() => this.setState({errorMessage: ''})}
-                            ></i>
-                            {errorMessage}
-                        </div>
-                    }
-                </div>
-                <div className="content">
-                    <AppInfo allMoviesCount={allMoviesCount} favouriteMoviesCount={favouriteMoviesCount} />
-                    <div className="search-panel">
-                        <SearchPanel updateTermHandler={updateTermHandler} />
-                        <AppFilter filter={filter} updateFilterHandler={updateFilterHandler} />
-                    </div>
-                    {/* <MovieList data={data} onDelete={onDelete} onToggleFavourite={onToggleFavourite} onToggleLike={onToggleLike} /> */}
-                    {/* <MovieList data={data} onDelete={onDelete} onToggleProp={onToggleProp} /> */}
-                    {
-                        finalNoResultMessage && filteredByData.length === 0 
-                        ? (<div className="fs-5 result-message">{finalNoResultMessage}</div>) 
-                        : <MovieList data={visibleData} onDelete={onDelete} onToggleProp={onToggleProp} />
-                    }
+//         return (
+//             <div className="app font-monospace" onClick={() => this.setState({errorMessage: ''})}>
+//                 <div className={`error-message ${errorMessage ? 'd-flex' : 'd-none'}`}>
+//                     {errorMessage && 
+//                         <div className="fs-4 message" onClick={(e) => e.stopPropagation()}>
+//                             <i 
+//                                 className="fa-solid fa-x fs-6 text-white" 
+//                                 onClick={() => this.setState({errorMessage: ''})}
+//                             ></i>
+//                             {errorMessage}
+//                         </div>
+//                     }
+//                 </div>
+//                 <div className="content">
+//                     <AppInfo allMoviesCount={allMoviesCount} favouriteMoviesCount={favouriteMoviesCount} />
+//                     <div className="search-panel">
+//                         <SearchPanel updateTermHandler={updateTermHandler} />
+//                         <AppFilter filter={filter} updateFilterHandler={updateFilterHandler} />
+//                     </div>
+//                     {/* <MovieList data={data} onDelete={onDelete} onToggleFavourite={onToggleFavourite} onToggleLike={onToggleLike} /> */}
+//                     {/* <MovieList data={data} onDelete={onDelete} onToggleProp={onToggleProp} /> */}
+//                     {
+//                         finalNoResultMessage && filteredByData.length === 0 
+//                         ? (<div className="fs-5 result-message">{finalNoResultMessage}</div>) 
+//                         : <MovieList data={visibleData} onDelete={onDelete} onToggleProp={onToggleProp} />
+//                     }
                     
-                    <MoviesAddForm addForm={addForm} />
-                </div>
-            </div>
-        );
-    }
-}
+//                     <MoviesAddForm addForm={addForm} />
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
