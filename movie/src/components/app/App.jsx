4004bdaeaf9ad7774381cee9ad75a57c71cad427
AkobirCoder <akobir0308@gmail.com>
2025-12-06 23:26:36 +0500

    ./movie/src/components/app/App.jsx was updated

--- movie/src/components/app/App.jsx
+++ movie/src/components/app/App.jsx
@@ -68,6 +68,21 @@ const App = () => {
             return item.name.toLowerCase().indexOf(term) > -1;
         });
     }
+    
+    const filterHandler = (array, filter) => {
+        switch (filter) {
+            case 'popular':
+                return array.filter((item) => {
+                    return item.like
+                });
+            case 'mostViewers':
+                return array.filter((item) => {
+                    return item.viewers > 800;
+                });
+            default:
+                return array;
+        }
+    }
 }
 
 class App extends Component {
