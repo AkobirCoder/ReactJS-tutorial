d3883e9dade6c8e5d592d90379fd7d71709ecb51
AkobirCoder <akobir0308@gmail.com>
2025-12-07 15:48:27 +0500

    ./movie/src/components/movies-add-form/Movies-add-form.jsx was updated

--- movie/src/components/movies-add-form/Movies-add-form.jsx
+++ movie/src/components/movies-add-form/Movies-add-form.jsx
@@ -1,4 +1,5 @@
-import { Component, useState } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 import './Movies-add-form.scss';
 
 const MoviesAddForm = ({addForm}) => {
