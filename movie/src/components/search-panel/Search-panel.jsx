abe86fda047fe0a4617cc0c043d5f991ec35aa89
AkobirCoder <akobir0308@gmail.com>
2025-12-07 16:00:32 +0500

    ./movie/src/components/search-panel/Search-panel.jsx was updated

--- movie/src/components/search-panel/Search-panel.jsx
+++ movie/src/components/search-panel/Search-panel.jsx
@@ -1,33 +1,56 @@
 import { Component } from 'react';
+import { useState } from 'react';
 import './Search-panel.scss';
 
-class SearchPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: '',
-        }
+const SearchPanel = (props) => {
+    const [term, setTerm] = useState('');
+
+    const updateTermHandler = (event) => {
+        const term = event.target.value.toLowerCase();
+
+        setTerm(term);
+
+        props.updateTermHandler(term);
     }
 
-    updateTermHandler = (e) => {
-        const term = e.target.value.toLowerCase();
-
-        this.setState({term});
-
-        const {updateTermHandler} = this.props;
-
-        updateTermHandler(term);
-    }
-
-    render() {
-        const {term} = this.state;
-        const {updateTermHandler} = this;
-
-        return (
-            <input type="text" className="form-control search-input" placeholder="Kinolarni qidirish" onChange={updateTermHandler} value={term} />
-        );
-    }
+    return (
+        <input 
+            type="text" 
+            className="form-control search-input" 
+            placeholder="Kinolarni qidirish" 
+            onChange={updateTermHandler} 
+            value={term} 
+        />
+    );
 }
+
+// class SearchPanel extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             term: '',
+//         }
+//     }
+
+//     updateTermHandler = (e) => {
+//         const term = e.target.value.toLowerCase();
+
+//         this.setState({term});
+
+//         const {updateTermHandler} = this.props;
+
+//         updateTermHandler(term);
+//     }
+
+//     render() {
+//         const {term} = this.state;
+//         const {updateTermHandler} = this;
+
+//         return (
+//             <input type="text" className="form-control search-input" placeholder="Kinolarni qidirish" onChange={updateTermHandler} value={term} />
+//         );
+//     }
+// }
 
 // const SearchPanel = () => {
 //     return (
