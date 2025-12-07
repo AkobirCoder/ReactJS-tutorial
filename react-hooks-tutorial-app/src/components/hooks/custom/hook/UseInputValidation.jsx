ec01fef99af6e5311b53dda22c981cf83a2cc7ca
AkobirCoder <akobir0308@gmail.com>
2025-12-07 23:25:45 +0500

    ./react-hooks-tutorial-app/src/components/hooks/custom/hook/UseInputValidation.jsx was added

---
+++ react-hooks-tutorial-app/src/components/hooks/custom/hook/UseInputValidation.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const useInputValidation = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    const validateInput = () => {
+        return value.search(/\d/) >= 0 ? true : false;
+    }
+
+    const validateColor = validateInput() ? 'text-danger' : null;
+
+    return {value, onChange, validateInput, validateColor};
+}
+
+export default useInputValidation;
