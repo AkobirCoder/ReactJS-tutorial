2df36b495b65a5a4ff7a505c3d1e48b382a06c28
AkobirCoder <akobir0308@gmail.com>
2025-12-07 14:34:41 +0500

    ./movie/src/components/movie-list/Movie-list.jsx was updated

--- movie/src/components/movie-list/Movie-list.jsx
+++ movie/src/components/movie-list/Movie-list.jsx
@@ -23,41 +23,41 @@ const MovieList = ({data, onDelete, onToggleProp}) => {
     );
 }
 
-class MovieList extends Component { 
-    constructor(props) {
-        super(props);
-        this.state = {
+// class MovieList extends Component { 
+//     constructor(props) {
+//         super(props);
+//         this.state = {
 
-        }
-    }
+//         }
+//     }
 
-    render() {
-        // const {data, onDelete, onToggleFavourite, onToggleLike} = this.props;
-        const {data, onDelete, onToggleProp} = this.props;
-        // console.log(data);
+//     render() {
+//         // const {data, onDelete, onToggleFavourite, onToggleLike} = this.props;
+//         const {data, onDelete, onToggleProp} = this.props;
+//         // console.log(data);
 
-        return (
-            <ul className='movie-list'>
-                {
-                    data.map(item => (
-                        <MovieListItem 
-                            key={item.id} 
-                            name={item.name} 
-                            viewers={item.viewers} 
-                            favourite={item.favourite} 
-                            like={item.like}
-                            onDelete={() => onDelete(item.id)}
-                            // onToggleFavourite={() => onToggleFavourite(item.id)}
-                            // onToggleLike={() => onToggleLike(item.id)}
-                            onToggleProp={(e) => onToggleProp(item.id, e.currentTarget.getAttribute('data-toggle'))}
-                        />
-                        // <MovieListItem {...item} />
-                    ))
-                }
-            </ul>
-        );
-    }
-}
+//         return (
+//             <ul className='movie-list'>
+//                 {
+//                     data.map(item => (
+//                         <MovieListItem 
+//                             key={item.id} 
+//                             name={item.name} 
+//                             viewers={item.viewers} 
+//                             favourite={item.favourite} 
+//                             like={item.like}
+//                             onDelete={() => onDelete(item.id)}
+//                             // onToggleFavourite={() => onToggleFavourite(item.id)}
+//                             // onToggleLike={() => onToggleLike(item.id)}
+//                             onToggleProp={(e) => onToggleProp(item.id, e.currentTarget.getAttribute('data-toggle'))}
+//                         />
+//                         // <MovieListItem {...item} />
+//                     ))
+//                 }
+//             </ul>
+//         );
+//     }
+// }
 
 // const MovieList = ({data, onDelete}) => {
 //     return (
