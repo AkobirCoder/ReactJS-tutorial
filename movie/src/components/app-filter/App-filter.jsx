f6e6df8f3098f910ed9bd839ff6b357557b21db8
AkobirCoder <akobir0308@gmail.com>
2025-12-07 15:02:52 +0500

    ./movie/src/components/app-filter/App-filter.jsx was updated

--- movie/src/components/app-filter/App-filter.jsx
+++ movie/src/components/app-filter/App-filter.jsx
@@ -1,43 +1,70 @@
 import { Component } from 'react';
 import './App-filter.scss'
 
-class AppFilter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+const AppFilter = ({updateFilterHandler, filter}) => {
+    const btnsArray = [
+        {name: 'all', label: "Barcha kinolar"},
+        {name: 'popular', label: "Mashhur kinolar"},
+        {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
+    ];
 
-        }
-    }
-
-    render() {
-        const btnsArray = [
-            {name: 'all', label: "Barcha kinolar"},
-            {name: 'popular', label: "Mashhur kinolar"},
-            {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
-        ];
-
-        const {updateFilterHandler, filter} = this.props;
-
-        return (
-            <div className="btn-group">
-                {
-                    btnsArray.map((btn) => {
-                        return (
-                            <button 
-                                className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
-                                type='button' 
-                                key={btn.name} 
-                                onClick={() => updateFilterHandler(btn.name)}
-                            >
-                                {btn.label}
-                            </button>
-                        );
-                    })
-                }
-            </div>
-        );
-    }
+    return (
+        <div className='btn-group'>
+            {
+                btnsArray.map((btn) => {
+                    return (
+                        <button 
+                            className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
+                            type='button'
+                            key={btn.name}
+                            onClick={() => updateFilterHandler(btn.name)}
+                        >
+                            {btn.label}
+                        </button>
+                    );
+                })
+            }
+        </div>
+    );
 }
+
+// class AppFilter extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+
+//         }
+//     }
+
+//     render() {
+//         const btnsArray = [
+//             {name: 'all', label: "Barcha kinolar"},
+//             {name: 'popular', label: "Mashhur kinolar"},
+//             {name: 'mostViewers', label: "Eng ko'p ko'rilgan kinolar"},
+//         ];
+
+//         const {updateFilterHandler, filter} = this.props;
+
+//         return (
+//             <div className="btn-group">
+//                 {
+//                     btnsArray.map((btn) => {
+//                         return (
+//                             <button 
+//                                 className={`btn ${filter === btn.name ? 'btn-dark' : 'btn-outline-dark'}`}
+//                                 type='button' 
+//                                 key={btn.name} 
+//                                 onClick={() => updateFilterHandler(btn.name)}
+//                             >
+//                                 {btn.label}
+//                             </button>
+//                         );
+//                     })
+//                 }
+//             </div>
+//         );
+//     }
+// }
 
 // const AppFilter = ({updateFilterHandler, filter}) => {
 //     const btnsArray = [
