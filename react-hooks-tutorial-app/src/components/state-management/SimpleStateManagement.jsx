c0ac5d60e63d56fff16628e1eef52392dc846f57
AkobirCoder <akobir0308@gmail.com>
2025-12-08 18:17:35 +0500

    ./react-hooks-tutorial-app/src/components/state-management/SimpleStateManagement.jsx was added

---
+++ react-hooks-tutorial-app/src/components/state-management/SimpleStateManagement.jsx
@@ -0,0 +1,38 @@
+import useInputValidation from "./custom-hook/UseInputValidation";
+
+const SimpleStateManagement = () => {
+    const firstName = useInputValidation('');
+    const lastName = useInputValidation('');
+
+    return (
+        <div className="container mb-5">
+            <code>simple State management:</code>
+            <div className="w-50 p-5 mt-5 mx-auto border rounded-3">
+                <h1 className="fs-3 fw-light text-center">
+                    simple State management
+                </h1>
+                <p className="text-center fs-3">Register</p>
+                <div className="d-flex gap-3">
+                    <input 
+                        type="text"
+                        className={`form-control ${firstName.validateColor}`}
+                        placeholder="Firstname"
+                        value={firstName.value}
+                        onChange={firstName.onChange}
+                    />
+                    <input 
+                        type="text"
+                        className={`form-control ${lastName.validateColor}`}
+                        placeholder="Lastname"
+                        value={lastName.value}
+                        onChange={lastName.onChange}
+                    />
+                </div>
+                <button className="mt-3 btn btn-success">Send data</button>
+            </div>
+            <hr />
+        </div>
+    );
+}
+
+export default SimpleStateManagement;
