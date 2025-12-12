719149cd27e58ffc01ce41ab15782ee31566af5e
AkobirCoder <akobir0308@gmail.com>
2025-12-12 14:56:34 +0500

    ./movie/src/index.js was updated

--- movie/src/index.js
+++ movie/src/index.js
@@ -2,10 +2,13 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './index.scss';
+import Provider from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <App />
+        <Provider>
+           <App /> 
+        </Provider>
     </StrictMode>
 );
