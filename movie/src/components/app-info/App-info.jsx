352e3d55fd0cb686057807a75b245fdd83b994bb
AkobirCoder <akobir0308@gmail.com>
2025-12-13 22:35:08 +0500

    ./movie/src/components/app-info/App-info.jsx was updated

--- movie/src/components/app-info/App-info.jsx
+++ movie/src/components/app-info/App-info.jsx
@@ -1,10 +1,21 @@
+import { useContext } from 'react';
+import { Context } from '../../context';
 import './App-info.scss';
 
-const AppInfo = ({allMoviesCount, favouriteMoviesCount}) => {
+// const AppInfo = ({allMoviesCount, favouriteMoviesCount}) => {
+const AppInfo = () => {
+    const {state} = useContext(Context);
+
+    const allMoviesCount = state.data.length;
+    const favouriteMoviesCount = state.data.filter((item) => item.favourite).length;
+
     return (
         <div className='app-info'>
             <p className='fs-3 text-uppercase'>Barcha kinolar soni: {allMoviesCount}</p>
             <p className='fs-4 text-uppercase'>Sevimli kinolar soni: {favouriteMoviesCount}</p>
+
+            {/* <p className='fs-3 text-uppercase'>Barcha kinolar soni: {state.data.length}</p>
+            <p className='fs-4 text-uppercase'>Sevimli kinolar soni: {state.data.filter((item) => item.favourite).length}</p> */}
         </div>
     );
 }
