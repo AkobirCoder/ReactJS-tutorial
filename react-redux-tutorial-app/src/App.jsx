7fee29024c88e5fdbb65b60ab86ee9ca20b5a201
AkobirCoder <akobir0308@gmail.com>
2025-12-30 23:46:30 +0500

    ./react-redux-tutorial-app/src/App.jsx was updated

--- react-redux-tutorial-app/src/App.jsx
+++ react-redux-tutorial-app/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
 
 const App = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount((prevState) => {
+            return prevState + 1;
+        });
+    }
+
+    const decrement = () => {
+        setCount((prevState) => {
+            return prevState - 1;
+        });
+    }
+
+    const reset = () => {
+        setCount(0);
+    }
+
     return (
-        <div className='container'>App</div>
+        <div className='container'>
+            <div className='d-flex flex-column align-items-center justify-content-center vh-100'>
+                <h1>Counter: {count}</h1>
+                <Counter increment={increment} decrement={decrement} reset={reset} />
+            </div>
+        </div>
     );
 }
 
