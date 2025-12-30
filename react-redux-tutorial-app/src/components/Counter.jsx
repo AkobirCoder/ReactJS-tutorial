0c7a2b7325c2917e074da688a500db43345b7df5
AkobirCoder <akobir0308@gmail.com>
2025-12-30 23:46:55 +0500

    ./react-redux-tutorial-app/src/components/Counter.jsx was added

---
+++ react-redux-tutorial-app/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PlusCounter from './PlusCounter';
+import MinusCounter from './MinusCounter';
+import ResetCounter from './ResetCounter';
+
+const Counter = ({increment, decrement, reset}) => {
+    return (
+        <div className='btn-group'>
+            <PlusCounter increment={increment} />
+            <MinusCounter decrement={decrement} />
+            <ResetCounter reset={reset} />
+        </div>
+    );
+}
+
+export default Counter;
