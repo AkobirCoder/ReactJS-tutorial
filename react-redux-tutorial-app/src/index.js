6abde326f145c1ac37859f9c45915adff7321f7b
AkobirCoder <akobir0308@gmail.com>
2025-12-31 16:17:53 +0500

    ./react-redux-tutorial-app/src/index.js was updated

--- react-redux-tutorial-app/src/index.js
+++ react-redux-tutorial-app/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
 );
