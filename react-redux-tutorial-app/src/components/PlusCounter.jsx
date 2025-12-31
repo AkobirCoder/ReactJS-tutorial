eb0cc2c7c88b21b46ffbeab2790a37d18f267724
AkobirCoder <akobir0308@gmail.com>
2025-12-31 16:18:45 +0500

    ./react-redux-tutorial-app/src/components/PlusCounter.jsx was updated

--- react-redux-tutorial-app/src/components/PlusCounter.jsx
+++ react-redux-tutorial-app/src/components/PlusCounter.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { plusCounter } from '../reducers/countSlice';
 
-const AddCounter = ({increment}) => {
+// const AddCounter = ({increment}) => {
+//     return (
+//         <button className='btn btn-success' onClick={increment}>Plus</button>
+//     );
+// }
+
+const AddCounter = () => {
+    const dispatch = useDispatch();
+
     return (
-        <button className='btn btn-success' onClick={increment}>Plus</button>
+        <button className='btn btn-success' onClick={() => dispatch(plusCounter())}>Plus</button>
     );
 }
 
