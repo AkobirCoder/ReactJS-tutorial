6d5dd647c0f42f65b97a1aaad271032f2153832f
AkobirCoder <akobir0308@gmail.com>
2025-12-31 16:17:34 +0500

    ./react-redux-tutorial-app/src/App.jsx was updated

--- react-redux-tutorial-app/src/App.jsx
+++ react-redux-tutorial-app/src/App.jsx
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import {useSelector} from 'react-redux';
 import Counter from './components/Counter';
 
 const App = () => {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
 
-    const increment = () => {
-        setCount((prevState) => {
-            return prevState + 1;
-        });
-    }
+    // const increment = () => {
+    //     setCount((prevState) => {
+    //         return prevState + 1;
+    //     });
+    // }
 
-    const decrement = () => {
-        setCount((prevState) => {
-            return prevState - 1;
-        });
-    }
+    // const decrement = () => {
+    //     setCount((prevState) => {
+    //         return prevState - 1;
+    //     });
+    // }
 
-    const reset = () => {
-        setCount(0);
-    }
+    // const reset = () => {
+    //     setCount(0);
+    // }
+
+    const state = useSelector(state => state.count);
+
+    console.log(state);
 
     return (
         <div className='container'>
             <div className='d-flex flex-column align-items-center justify-content-center vh-100'>
-                <h1>Counter: {count}</h1>
-                <Counter increment={increment} decrement={decrement} reset={reset} />
+                {/* <h1>Counter: {count}</h1>
+                <Counter increment={increment} decrement={decrement} reset={reset} /> */}
+
+                {/* React redux orqali state management qilish: */}
+                <h1>Counter: {state}</h1>
+                <Counter />
             </div>
         </div>
     );
