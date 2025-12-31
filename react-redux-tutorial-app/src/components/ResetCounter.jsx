1f19b63f670b76e0a84893a8ec07080b1ce41f7b
AkobirCoder <akobir0308@gmail.com>
2025-12-31 16:19:04 +0500

    ./react-redux-tutorial-app/src/components/ResetCounter.jsx was updated

--- react-redux-tutorial-app/src/components/ResetCounter.jsx
+++ react-redux-tutorial-app/src/components/ResetCounter.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { resetCounter } from '../reducers/countSlice';
 
-const ResetCounter = ({reset}) => {
+// const ResetCounter = ({reset}) => {
+//     return (
+//         <button className='btn btn-danger' onClick={reset}>Reset</button>
+//     );
+// }
+
+const ResetCounter = () => {
+    const dispatch = useDispatch();
+
     return (
-        <button className='btn btn-danger' onClick={reset}>Reset</button>
+        <button className='btn btn-danger' onClick={() => dispatch(resetCounter())}>Reset</button>
     );
 }
 
