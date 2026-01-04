d18443fec71316e35093916e72adcdeb9475a5ad
AkobirCoder <akobir0308@gmail.com>
2026-01-04 16:18:23 +0500

    ./movie/src/components/movies-add-form/Movies-add-form.jsx was updated

--- movie/src/components/movies-add-form/Movies-add-form.jsx
+++ movie/src/components/movies-add-form/Movies-add-form.jsx
@@ -114,20 +114,20 @@ const MoviesAddForm = ({addForm}) => {
 //     }
 // }
 
-// // const MoviesAddForm = () => {
-// //     return (
-// //         <div className='movies-add-form'>
-// //             <h3>Yangi kino qo'shish</h3>
-// //             <form className='add-form d-flex'>
-// //                 <input type="text" className='form-control mx-1 new-post-label' placeholder='Qanday kino?' />
-// //                 <input type="number" className='form-control mx-1 new-post-label' placeholder="Nechi marotaba ko'rilgan?" />
-// //                 <button className='btn btn-outline-dark ms-1' type='submit'>
-// //                     Qo'shish
-// //                 </button>
-// //             </form>
-// //         </div>
-// //     );
-// // }
+// const MoviesAddForm = () => {
+//     return (
+//         <div className='movies-add-form'>
+//             <h3>Yangi kino qo'shish</h3>
+//             <form className='add-form d-flex'>
+//                 <input type="text" className='form-control mx-1 new-post-label' placeholder='Qanday kino?' />
+//                 <input type="number" className='form-control mx-1 new-post-label' placeholder="Nechi marotaba ko'rilgan?" />
+//                 <button className='btn btn-outline-dark ms-1' type='submit'>
+//                     Qo'shish
+//                 </button>
+//             </form>
+//         </div>
+//     );
+// }
 
 export default MoviesAddForm;
 
